6c9b920e04475260bede63dd1b71425a57208a70
Harrinson Morales <harrinson.moralestejedor@unicolombo.edu.co>
2024-04-24 21:34:26 -0500

    Harri morales

---
+++ guia 17 gui/src/paqueteCalculos/calculos.java
@@ -0,0 +1,22 @@
+
+package paqueteCalculos;
+
+import paqueteCalculos.vista.ventanaCalculos;
+
+/**
+ *
+ * @author HarriMorales
+ */
+public class calculos {
+    
+public static void main(String[]args){
+    
+ventanaCalculos ventana = new ventanaCalculos();
+
+ventana.setLocationRelativeTo(null);
+ventana.setVisible(true);
+
+
+}
+    
+}
